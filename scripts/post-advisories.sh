00edf9980cbb151f9f304a70df5baf2950413285
Reed Loden <reed@reedloden.com>
2015-11-18 11:42:37 -0800

    Update Travis CI credentials and move git config settings to correct spot

--- scripts/post-advisories.sh
+++ scripts/post-advisories.sh
@@ -5,12 +5,12 @@ set -o errexit -o nounset
 REPO="https://${GH_TOKEN}@github.com/rubysec/rubysec.github.io.git"
 DIR="_site"
 
-git config user.name "RubySec CI"
-git config user.email "ci@rubysec.com"
-
 git clone $REPO $DIR
 
 cd $DIR
+
+git config user.name "RubySec CI"
+git config user.email "ci@rubysec.com"
 
 bundle install --jobs=3 --retry=3
 bundle exec rake advisories
