fbc1da0341dded444e8ef07dd8ee70fb9ffc3ae5
Ed Robinson <ed.robinson@reevoo.com>
2017-09-20 14:56:09 +0100

    Adds a test to ensure that patched_versions has an open ended upper limit
    Fixes #285

--- spec/gem_example.rb
+++ spec/gem_example.rb
@@ -3,7 +3,7 @@ require 'advisory_example'
 
 shared_examples_for "Gem Advisory" do |path|
   include_examples 'Advisory', path
-  
+
   advisory = YAML.load_file(path)
 
   describe path do
@@ -15,6 +15,20 @@ shared_examples_for "Gem Advisory" do |path|
       it { is_expected.to be_kind_of(String) }
       it "should be equal to filename (case-insensitive)" do
         expect(subject.downcase).to eq(gem.downcase)
+      end
+    end
+
+    describe "versions" do
+      it "assumes that future versions will be patched" do
+        unaffected_versions = advisory['unaffected_versions'] || []
+        patched_versions    = advisory['patched_versions'] || []
+
+        versions  = unaffected_versions + patched_versions
+
+        # If a gem is unpatched this test makes no sense
+        unless patched_versions.none?
+          expect(versions.any? { |version| version.match(/^>=|^>/)}).to be_truthy
+        end
       end
     end
   end
