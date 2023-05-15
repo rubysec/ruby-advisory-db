b45f82ef85c32a1354a8eaeca62da047247144c9
Postmodern <postmodern.mod3@gmail.com>
2023-05-14 21:15:41 -0700

    Added a `ROOT` constant.

--- spec/advisories_spec.rb
+++ spec/advisories_spec.rb
@@ -3,13 +3,13 @@ require 'gem_advisory_example'
 require 'ruby_advisory_example'
 
 describe "gems" do
-  Dir.glob(File.join(__dir__, '../gems/*/*')) do |path|
+  Dir.glob(File.join(ROOT,'gems/*/*')) do |path|
     include_examples 'Gem Advisory', path
   end
 end
 
 describe "rubies" do
-  Dir.glob(File.join(__dir__, '../rubies/*/*')) do |path|
+  Dir.glob(File.join(ROOT, 'rubies/*/*')) do |path|
     include_examples 'Rubies Advisory', path
   end
 end
