6ff520896a8f5bd33ab562653e87ab974d10b55b
Al Snow <jasnow@hotmail.com>
2023-06-18 18:13:15 -0400

    Added fix as requested in PR feedback

--- spec/advisory_example.rb
+++ spec/advisory_example.rb
@@ -94,7 +94,7 @@ shared_examples_for 'Advisory' do |path|
       subject { advisory['url'] }
 
       it { expect(subject).to be_kind_of(String) }
-      it { expect(subject).not_to include("osvdb") }
+      it { expect(subject).not_to include("http://osvdb.org") }
       it { expect(subject).not_to be_empty }
     end
 
